f5f46a8d4cd7d6bf29fd9164b35f6228a15aa344
Oleg721 <novikov.oleg721@gmail.com>
2021-12-31 12:06:41 +0200

    Add project files.

---
+++ Api/Model/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Model
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string UserId { get; set;}
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Api/SignalRHub/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace Api.SignalRHub
+{
+    [AllowAnonymous]
+    public class ChatHub : Hub
+    {
+
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("Connected");
+            return Task.CompletedTask;
+        }
+
+
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Send", message);
+        }
+    }
+}
